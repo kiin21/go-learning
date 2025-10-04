86221af8a9b55a841b3283678f2721be43e0bbfb
kiin21 <pdanhkhoa21@gmail.com>
2025-10-04 08:34:19 +0700

    data race

---
+++ go-goroutine/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var mu sync.Mutex
+var count int
+
+func increment() {
+	mu.Lock()
+	count++
+	mu.Unlock()
+}
+
+func dataRace() {
+	var wg sync.WaitGroup
+
+	startTime := time.Now()
+
+	// Data race and fix with mutex
+	for i := 0; i < 1000000; i++ {
+		wg.Add(1)
+		go func() {
+			increment()
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+	duration := time.Since(startTime).Milliseconds()
+
+	fmt.Printf("Count: %d, Time executed: %d ms\n", count, duration)
+}
+
+func main() {
+	dataRace()
+}
